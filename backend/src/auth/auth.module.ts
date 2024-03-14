883fe710068c95714aa6c5ed51e27fa36aeaba71
Milen <feniks9511@gmail.com>
2024-03-14 14:14:35 +0200

    refacturing and properly using .env parameters

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,14 +6,21 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UserModule } from 'src/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule,
+  imports: [UserModule,ConfigModule.forRoot({
+    isGlobal: true,
+  }),
     PassportModule,
-    JwtModule.register({
-      secret: 'darkHorse',
+    JwtModule.registerAsync({
+      imports: [ConfigModule], 
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), 
       signOptions: { expiresIn: '1h' },
     }),
+  }),
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'darkHorse',
+      secretOrKey: configService.get<string>('JWT_SECRET'), // Load JWT secret from environment variable
     });
   }
 
